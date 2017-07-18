8694071e462c9212853d68e479056575e4db8dc1
Ryan <james.ryan01@sap.com>
2017-07-18 16:42:08 -0700

    Fix from seconds to milliseconds for time

--- priv/assets/js/scripts.js
+++ priv/assets/js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		$.get( server_address + "/tables", function( data ) {
 			console.log(data);
 			let latest_activity = data.status.latest_activity;
-			var d = new Date(latest_activity);
+			var d = new Date(latest_activity * 1000);
 			$("#lastKnownActivity").html(d.getHours() + ":" + d.getMinutes() + " " + d.toDateString());
 		});
 		
